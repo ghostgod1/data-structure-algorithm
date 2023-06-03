d37b47a583f829a8f6dfc9e87422b5eba7112759
ghostgod1 <73012930+ghostgod1@users.noreply.github.com>
2023-06-03 23:20:11 +0530

    revision

--- c and c++ essentials for dsa/04_refrence.cpp
+++ c and c++ essentials for dsa/04_refrence.cpp
@@ -3,7 +3,16 @@
 using namespace std;
 int main()
 {
+    // * before the variable then it is pointer/address variable
+    // & before the variable then it is refrence variable 
+    // nothing before the variable then it is data variable
+
+
     int a = 10;
+
+    /*refrence is just another name for a variable 
+    it is used for parameter passing */
+    //refrence must be initialized when declared
     int &r = a; //refrence variabel
 
     cout<<a<<"\n";
--- c and c++ essentials for dsa/05_0_pointer_in_structure.c
+++ c and c++ essentials for dsa/05_0_pointer_in_structure.c
@@ -11,8 +11,10 @@ int main()
     struct rectangle *p = &r;    //defining pointer for structure
     
     r.length = 15;
+
     (*p).length = 16;   //method for calling pointer 
     p->length = 17;     //another method for calling pointer
+    p->breadth = 12;
 
     printf("%d",*p);
 }
